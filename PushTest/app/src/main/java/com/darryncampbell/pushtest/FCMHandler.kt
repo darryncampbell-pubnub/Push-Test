4401ff3f3810abb2ecc6d2797a3821b3dad02241
darryn.campbell <darryn.campbell@pubnub.com>
2024-05-01 09:23:25 +0100

    simple demo app

---
+++ PushTest/app/src/main/java/com/darryncampbell/pushtest/FCMHandler.kt
@@ -0,0 +1,78 @@
+package com.darryncampbell.pushtest
+
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.os.IBinder
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class FCMHandler : FirebaseMessagingService() {
+    private val LOG_TAG = "PushTest"
+
+    override fun onNewToken(token: String) {
+        //Called whenever the FCM token is renewed - re-register the device with PubNub
+        return
+        Log.d(LOG_TAG, "New Token Received: $token")
+    }
+
+    //Handle when the device has received a push notification from FCM.
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        super.onMessageReceived(remoteMessage)
+        val clientIdentifier = applicationContext.getSharedPreferences("prefs.db", 0).getString("identifier", null)
+        var title : String
+        var body : String
+
+        Log.d(LOG_TAG, "Message Received from FCM")
+
+        // Check if message contains a data payload.
+        //  This is always handled by the application regardless of foreground / background.
+        //  Display the notification ourselves
+        if (remoteMessage.data.isNotEmpty()) {
+            remoteMessage.data.let {
+                Log.d(LOG_TAG, "Message data payload: " + remoteMessage.data)
+                title = remoteMessage.data.get("title").toString()
+                body = remoteMessage.data.get("body").toString()
+                sendNotification(title, body)
+            }
+        }
+
+        // Check if message contains a notification payload.
+        //  This will never be the case when the app is in the background (since the
+        //  notification is handled automatically by FCM).
+        //  If we are in the foreground, display a snackbar rather than a notification
+        remoteMessage.notification?.let {
+            Log.d(LOG_TAG, "Message Notification Body: ${it.body}")
+            title = remoteMessage.notification?.title.toString()
+            body = remoteMessage.notification?.body.toString()
+
+            sendNotification(title, body)
+        }
+    }
+
+    private fun sendNotification(title: String, body: String) {
+        val notificationID = 101
+        //  Just launch the app if the notification is tapped
+        val intent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pend = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE)
+        val channelID = getString(R.string.default_notification_channel_id)
+
+        var builder = NotificationCompat.Builder(this, channelID)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle(title)
+            .setContentText(body)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setColor(resources.getColor(R.color.purple_500, theme))
+            .setContentIntent(pend)
+            .setAutoCancel(true)
+        val notificationManager: NotificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(notificationID, builder.build())
+    }
+}
---
+++ PushTest/app/src/main/java/com/darryncampbell/pushtest/MainActivity.kt
@@ -0,0 +1,106 @@
+package com.darryncampbell.pushtest
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.text.SpannableStringBuilder
+import android.util.Log
+import android.widget.TextView
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.text.bold
+import androidx.core.text.underline
+import com.darryncampbell.pushtest.ui.theme.PushTestTheme
+import com.google.firebase.messaging.FirebaseMessaging
+
+class MainActivity : ComponentActivity() {
+    private val LOG_TAG = "PushTest"
+    private var pushNotificationPermission = true
+    private val pushNotificationPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { granted ->
+        if (!granted)
+        {
+            Log.e(LOG_TAG, "Notification permissions were not granted")
+            pushNotificationPermission = false
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        createNotificationChannel()
+        getToken()
+        pushNotificationPermissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS)
+        setContent {
+            PushTestTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+
+    private fun getToken()
+    {
+        FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
+            try {
+                val token = task.result
+                if (token != null) {
+                    Log.d(LOG_TAG, "Retrieved token: $token");
+                }
+            } catch (e: Exception) {
+                //  This issue seems to happen on a newly launched emulator through Appetize.io ('switching to device').  Seems to be a known issue.
+                //  https://stackoverflow.com/questions/62562243/java-io-ioexception-authentication-failed-in-android-firebase-and-service-not
+                Handler(Looper.getMainLooper()).postDelayed(Runnable { getToken(); }, 1000)
+            }
+        }
+    }
+
+    private fun createNotificationChannel() {
+        val notificationManager: NotificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.default_notification_channel_name)
+            val descriptionText = getString(R.string.default_notification_channel_description)
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val channel = NotificationChannel( getString(R.string.default_notification_channel_id),name, importance).apply {
+                description = descriptionText
+            }
+            //Channel settings
+            channel.description = descriptionText
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PushTestTheme {
+        Greeting("Android")
+    }
+}
